39a9df3305e7cb36226502b8d7eac8ae652b7382
Sheldon McGrandle <developer@rednemesis.com>
2012-12-16 13:07:02 -0500

    Beginning of the licence and preamble text

--- show_css.el
+++ show_css.el
@@ -1,3 +1,35 @@
+;;; COPYRIGHT NOTICE
+;;
+;; Copyright (C) 2012 Sheldon McGrandle.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; author of this program <mboyer@ireq-robot.hydro.qc.ca> or to the
+;; Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;; Send bug reports to Sheldon McGrandle <sheldon@rednemesis.com>
+
+
+;;; DESCRIPTION AND USAGE
+;;
+
+;;; INSTALLATION
+;;
+
+;;; BUGS
+;;
+
+
 ;; multiple classes? class="dog cat cow"
 ;;
 
