05c12dbd586a6de727777d5157b82c7b67d366d6
Sheldon McGrandle <developer@rednemesis.com>
2013-07-30 10:57:49 -0400

    Probable end of experimental websocket branch

--- webkit-debug.el
+++ webkit-debug.el
@@ -28,6 +28,7 @@
 ;;; Code:
 
 ;;https://www.webkit.org/blog/1875/announcing-remote-debugging-protocol-v1-0/
+;;http://trac.webkit.org/export/129139/trunk/Source/WebCore/inspector/Inspector.json
 ;;https://developers.google.com/chrome-developer-tools/docs/protocol/1.0/
 
 (require 'json)
@@ -42,19 +43,37 @@
 (defvar wk-source-file "file:///"
   "File to debug")  ;FIXME: remove later
 
+(defvar wk-debug-buffer "Webkit debug buffer")
+(defvar wk-socket nil)
+
+
+(defun wk-get-data()
+  (wk-send "Page.enable" :id 1)
+  (wk-send "CSS.enable" :id 2)
+
+  ;(wk-send "Page.reload" :id 1 :params (list :ignoreCache t))
+  ;;(wk-send "DOM.getDocument" :id 2)
+  ;(wk-send "DOM.requestChildNodes" :id 21 :params (list :NodeId 46))
+  ;(wk-send "CSS.getAllStyleSheets" :id 3) x
+  ;(wk-send "DOM.performSearch" :id 4 :params (list :query "showcss"))
+  ;(wk-send "Page.getResourceTree" :id 5)
+)
+
+
 ;(wk-connect "/home/sm/projects/show_css/test/index.html")
 (defun wk-connect(file)
   "Connect to a running webkit that was started with --remote-debugging-port=9222"
-  (let ((url-request-method "GET")
-        (url-package-name "webkit-debug.el")
-        (url-package-version "0.1")
-        (url-http-attempt-keepalives nil)
-        (url (url-parse-make-urlobj
-              "http" nil nil wk-host wk-port "/json"))
-        (file-url (concat "file://" (expand-file-name file))))
+  (let* ((url-request-method "GET")
+         (url-package-name "webkit-debug.el")
+         (url-package-version "0.1")
+         (url-http-attempt-keepalives nil)
+         (url (url-parse-make-urlobj
+               "http" nil nil wk-host wk-port "/json"))
+         (file-url (concat "file://" (expand-file-name file)))
+         (url-retrieve-buffer (url-retrieve-synchronously url)))
     ;; connect to the webkit, get the json, and switch
     ;; to that buffer and read the json
-    (with-current-buffer (url-retrieve-synchronously url)
+    (with-current-buffer url-retrieve-buffer
       (goto-char 0)
       (if (and (looking-at "HTTP/1\\.. 200")
                (re-search-forward "\n\n" nil t))
@@ -71,52 +90,53 @@
             (if (not tab)
                 (error "No Chrome tab found for %s" file-url))
 
-            (setq socket (websocket-open
-                          (plist-get tab :webSocketDebuggerUrl)
-                          :on-open (lambda (websocket)
-                                     (message "Websocket opened"))
-                          :on-message (function wk-recieve)
-                          :on-close (lambda (websocket)
-                                      (message "Websocket closed"))))
-
-            (pop-to-buffer "Websocket Output")
-            (wk-send socket "DOM.getDocument")
-            (websocket-close socket)
-            )
+            (setq wk-socket (websocket-open
+                             (plist-get tab :webSocketDebuggerUrl)
+                             :on-open (lambda (websocket)
+                                        (message "Websocket opened"))
+                             :on-message (function wk-recieve)
+                             :on-close (lambda (websocket)
+                                      (message "Websocket closed")))))
 
         (error "Could not contact remote debugger at %s:%s"
                wk-host
-               wk-port)))))
+               wk-port)))
+    ;(kill-buffer url-retrieve-buffer)
+    ))
 
 
 
 (defun wk-recieve (socket frame)
-  (with-temp-buffer
-    (insert (format "%s" socket))
+  ;(switch-to-buffer wk-debug-buffer)
+  (with-current-buffer wk-debug-buffer
+    (goto-char (point-min))
+    ;(insert (pp-to-string (json-read-from-string (websocket-frame-payload frame))))
+    (insert (pp-to-string (websocket-frame-payload frame)))
+
+    ;;(let ((action-id (cdr (car (json-read-from-string (websocket-frame-payload frame))))))
+    ;;  (if (= action-id 2)
+    ;;      (let ((node-id (cdr (nth 5 (cdr (cdr (elt (cdr (nth 3 (cdr (car (cdr (car (cdr (json-read-from-string (websocket-frame-payload frame))))))))) 0)))))))
+    ;;        (wk-send "DOM.requestChildNodes" :id 99999 :params (list :NodeId node-id)))
+    ;;    ))
+
     (insert "\n\n")
-    (insert (websocket-frame-payload frame))
-    (insert "\n\n")
-    (insert (format "%s" frame))
-    (insert "\n\n")
-    (insert "\n\n")
-))
+    ))
 
 
-(defun* wk-send (socket method &key params)
-
-  (unless (websocket-openp socket)
-    (error "Websocket gone, why?: %s" socket))
+(defun* wk-send (method &key params id)
+  (unless (websocket-openp wk-socket)
+    (wk-connect "/home/sm/projects/show_css/test/index.html"))
 
   ;; build json for browser
   (let ((request (json-encode
                   (list
                    (cons :jsonrpc "2.0")
-                   (cons :id 1)
+                   (cons :id id)
                    (cons :method method)
                    (cons :params params)))))
 
-    (websocket-send-text socket request)
-))
+    (websocket-send-text wk-socket request)
+  ))
 
 
 
@@ -125,3 +145,40 @@
 
 (provide 'webkit-debug)
 ;;; webkit-debug.el ends here
+
+
+;;(defun wss-recieve (websocket frame)
+;;  ;(pop-to-buffer "Test websocket output")
+;;  (with-current-buffer "Test websocket output"
+;;    ;(delete-region (point-min) (point-max))
+;;    (insert
+;;     (pp-to-string
+;;      (json-read-from-string (websocket-frame-payload frame))))
+;;    ;(websocket-close websocket)
+;;  ))
+;;
+;;(setq wss-socket (websocket-open
+;;                  "ws://127.0.0.1:9222/devtools/page/3_1"
+;;                  :on-message (function wss-recieve)))
+;;
+;;
+;;
+;;(progn
+;;  (let ((js-doc (json-encode
+;;                 (list
+;;                  (cons :jsonrpc "2.0")
+;;                  (cons :id 12)
+;;                  (cons :method "DOM.getDocument")
+;;                  (cons :params nil)
+;;                    )))
+;;        (js (json-encode
+;;             (list
+;;              (cons :jsonrpc "2.0")
+;;              (cons :id 12)
+;;              (cons :method "DOM.requestNode")
+;;              (cons :params
+;;                     (list :objectId "136"))
+;;                    ))))
+;;    (print js)
+;;    (websocket-send-text wss-socket js-doc)
+;;    (websocket-send-text wss-socket js)))
